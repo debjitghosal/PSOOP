96ce25e6c170e7439665f7519f9af43555fdfeb0
debjitghosal <155469474+debjitghosal@users.noreply.github.com>
2024-03-12 19:06:02 +0530

    Update Stock1.java

--- Inheritance/Stock1.java
+++ Inheritance/Stock1.java
@@ -1,3 +1,18 @@
+/*
+PROBLEM STATEMENT :	The cost of stock on each day is given in an array A[] of size N. Day 1 price in first location, day 2 price in
+second location etc. Find all the days on which you buy and sell the stock any number of times so that in
+between those days your profit is maximum.A new transaction can only start after the previous
+transaction is complete. Person can hold only one share at a time. Create a class Stock that has the name
+of stock and array of prices. Also it has input method that initialises the predicted price of the stock in an
+array of length N. Create class Transaction that is a sub class of Stock class. It has method
+findMaximumProfit method.
+Example Stock Prices: {1, 5, 2, 3, 7, 6, 4, 5}
+Total profit earned is 10
+Buy on day 1 and sell on day 2
+Buy on day 3 and sell on day 5
+Buy on day 7 and sell on day 8
+*/
+
 import java.util.*;
 
 class Stock {
